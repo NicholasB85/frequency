f66a89aaf42f6920e0bd5da8dd16a19e49b7c954
NicholasB85 <nicholas.w.burkhart@gmail.com>
2018-05-02 11:13:19 -0400

    not completely finished, dont know how to calculate words

---
+++ main.js
@@ -0,0 +1,40 @@
+
+
+document.getElementById("countButton").onclick = function() {
+    var typedText = document.getElementById("textInput").value;
+
+ typedText = typedText.toLowerCase();
+// This changes all the letter to lower case.
+
+typedText = typedText.replace(/[^a-z'\s]+/g, "");
+// This gets rid of all the characters except letters, spaces, and apostrophes.
+// We will learn more about how to use the replace function in an upcoming lesson.
+
+letterCounts = {};//use the idea of associative arrays from the lesson
+wordCounts = {};
+
+for(let i = 0; i < typedText.length; i++) {
+    currentLetter = typedText[i];
+
+    if(letterCounts[currentLetter] === undefined) {
+        letterCounts[currentLetter] = 1;
+     } else {
+        letterCounts[currentLetter]++;
+     }
+    // do something for each letter.
+ }
+
+ for(letter in letterCounts) {
+    let span = document.createElement("span");
+    let textContent = document.createTextNode('"' + letter + "\": " + letterCounts[letter] + ", ");
+    span.appendChild(textContent);
+    document.getElementById("lettersDiv").appendChild(span);
+ }
+
+
+ words = typedText.split(/\s/);
+ }
+
+
+ 
+ 
